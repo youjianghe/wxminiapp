a0bb0662bcaf260039f279ae48f9a6be68433fb2
youjianghe <youjianghe@foxmail.com>
2017-07-25 10:15:00 +0800

    create

---
+++ README.md
@@ -0,0 +1,2 @@
+# aipinbaobao
+微信小程序练习
---
+++ pages/index/index.js
@@ -0,0 +1,11 @@
+//index.js
+//获取应用实例
+var app = getApp();
+Page({
+    data:{
+        imgurls:[
+            "/images/banner1.png",
+            "/images/banner2.png"
+        ]
+    }
+})
